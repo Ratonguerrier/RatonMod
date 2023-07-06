44bdc7117c5182d2758dc476acb1c531f44e2004
RatonGuerrier <antoninm0414@gmail.com>
2023-07-06 15:40:13 +0200

    update informations of mod

--- src/main/java/turniplabs/ratonmod/RatonMod.java
+++ src/main/java/turniplabs/ratonmod/RatonMod.java
@@ -1,16 +1,17 @@
-package turniplabs.examplemod;
+package turniplabs.ratonmod;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-public class ExampleMod implements ModInitializer {
-    public static final String MOD_ID = "examplemod";
+public class RatonMod implements ModInitializer
+{
+    public static final String MOD_ID = "ratonmod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
-        LOGGER.info("ExampleMod initialized.");
+        LOGGER.info("RatonMod initialized.");
     }
 }
